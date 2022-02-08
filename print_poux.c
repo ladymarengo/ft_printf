31d9751c5769a3d5cd9ca991b409214e29c593a3
Natalia Samoilova <samoilova.natalia.n@gmail.com>
2022-02-08 11:22:49 +0200

    Another malloc protection

--- print_poux.c
+++ print_poux.c
@@ -6,15 +6,14 @@
 /*   By: nsamoilo <nsamoilo@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/26 17:39:55 by nsamoilo          #+#    #+#             */
-/*   Updated: 2022/02/07 15:57:51 by nsamoilo         ###   ########.fr       */
+/*   Updated: 2022/02/08 11:19:55 by nsamoilo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-void	handle_u_precision(t_tags *tags, char **str, uintmax_t number)
+void	handle_u_precision(t_tags *tags, char **str, uintmax_t number, int add)
 {
-	int		add;
 	char	*add_str;
 	char	*temp;
 
@@ -26,6 +25,8 @@ void	handle_u_precision(t_tags *tags, char **str, uintmax_t number)
 		if (add > 0)
 		{
 			add_str = ft_strnew(add);
+			if (!add_str)
+				error_exit(tags, 1, *str);
 			ft_memset(add_str, '0', add);
 			temp = ft_strjoin(add_str, *str);
 			free(*str);
@@ -38,6 +39,25 @@ void	handle_u_precision(t_tags *tags, char **str, uintmax_t number)
 			*str = ft_strdup("");
 		}
 	}
+}
+
+char	*create_string(t_tags *tags, uintmax_t number)
+{
+	char	*str;
+
+	if (tags->specifier == 'u')
+		str = ft_itoa_base(number, 10);
+	else if (tags->specifier == 'o')
+		str = ft_itoa_base(number, 8);
+	else if (tags->specifier == 'p' && number == 0)
+		str = ft_strdup("(nil)");
+	else if (ft_tolower(tags->specifier) == 'x' || tags->specifier == 'p')
+		str = ft_itoa_base(number, 16);
+	if (tags->specifier == 'X')
+		ft_str_toupper(str);
+	if (!str)
+		error_exit(tags, 0);
+	return (str);
 }
 
 void	print_poux(t_tags *tags, va_list args, int *chars)
@@ -55,16 +75,9 @@ void	print_poux(t_tags *tags, va_list args, int *chars)
 		number = (unsigned long)va_arg(args, unsigned long);
 	else
 		number = (unsigned int)va_arg(args, unsigned int);
-	if (tags->specifier == 'u')
-		str = ft_itoa_base(number, 10);
-	else if (tags->specifier == 'o')
-		str = ft_itoa_base(number, 8);
-	else if (tags->specifier == 'p' && number == 0)
-		str = ft_strdup("(nil)");
-	else if (ft_tolower(tags->specifier) == 'x' || tags->specifier == 'p')
-		str = ft_itoa_base(number, 16);
-	if (tags->specifier == 'X')
-		ft_str_toupper(str);
-	handle_u_precision(tags, &str, number);
+	str = create_string(tags, number);
+	handle_u_precision(tags, &str, number, 0);
+	if (!str)
+		error_exit(tags, 0);
 	print_left_or_right(&str, tags, chars);
 }
